eddb0b688bec11e91c2e0cd4c0e9274751f1a45b
Hkc-1673 <habibkc71@gmail.com>
2020-12-04 14:57:16 +0200

    Added database

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const port = process.env.port || 5000;
+
+const MainRouter = require("./routes/MainRouter")
+const UserRouter = require("./routes/UserRouter")
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", MainRouter);
+app.use("/user", UserRouter);
+
+app.listen(port, () => {
+  console.log(`I'm listening on ${port}`);
+});
---
+++ models/data.js
@@ -0,0 +1,8 @@
+exports.userList = [
+    {id:0, name:"John"},
+    {id:1, name:"Jane"},
+    {id:2, name:"Joe"},
+    {id:3, name:"Alison"},
+    {id:4, name:"Harold"},
+    {id:5, name:"Wayne"},
+];
